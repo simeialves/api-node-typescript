8586ddcf4695f904522cf28a019ce693f7a5cc47
Simei Alves <simeiparreiras@gmail.com>
2025-03-17 22:53:20 -0300

    feat: implementar controladores e rotas para operações CRUD de cidades

--- src/server/controllers/cidades/index.ts
+++ src/server/controllers/cidades/index.ts
@@ -1,5 +1,13 @@
 import * as create from "./Create.js";
+import * as deleteById from "./DeleteById.js";
+import * as getAll from "./GetAll.js";
+import * as getById from "./GetById.js";
+import * as updateById from "./UpdateById.js";
 
 export const CidadesController = {
   ...create,
+  ...getAll,
+  ...getById,
+  ...updateById,
+  ...deleteById,
 };
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -7,10 +7,33 @@ router.get("/", (_, res) => {
   return res.send("Olá Dev!");
 });
 
+router.get(
+  "/cidades",
+  CidadesController.getAllValidation,
+  CidadesController.getAll
+);
+
 router.post(
   "/cidades",
   CidadesController.createValidation,
   CidadesController.create
+);
+
+router.get(
+  "/cidades/:id",
+  CidadesController.getByIdValidation,
+  CidadesController.getById
+);
+
+router.put(
+  "/cidades/:id",
+  CidadesController.updateByIdValidation,
+  CidadesController.updateById
+);
+router.delete(
+  "/cidades/:id",
+  CidadesController.deleteByIdValidation,
+  CidadesController.deleteById
 );
 
 export { router };
